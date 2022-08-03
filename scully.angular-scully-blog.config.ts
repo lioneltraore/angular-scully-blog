940d0566753c68dbe62d789997578b5c5741c817
Lionel Traore <hello@lioneltraore.com>
2022-08-03 14:33:32 +0000

    display articles

--- scully.angular-scully-blog.config.ts
+++ scully.angular-scully-blog.config.ts
@@ -1,19 +1,19 @@
 import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
-import '@scullyio/scully-plugin-puppeteer'
+import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "angular-scully-blog",
+  projectRoot: './src',
+  projectName: 'angular-scully-blog',
   // add spsModulePath when using de Scully Platform Server,
   outDir: './dist/static',
   routes: {
     '/posts/:id': {
       type: 'contentFolder',
       id: {
-        folder: "./mdfiles"
-      }
+        folder: './mdfiles',
+      },
     },
-  }
+  },
 };
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  posts: ScullyRoute[] = [];
+  routeSub: Subscription | undefined;
+
+  constructor(private scullyService: ScullyRoutesService) { }
 
   ngOnInit(): void {
+    this.routeSub = this.scullyService.available$.subscribe(posts => {
+      this.posts = posts.filter(post => post.title);
+    })
+  }
+
+  ngOnDestroy() {
+    this.routeSub?.unsubscribe();
   }
 
 }
